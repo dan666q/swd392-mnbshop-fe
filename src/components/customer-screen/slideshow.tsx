6714ff597d6a56c3641b1884d45d85e4a8aa6266
ngtrungh1eu <hieuntse160479@fpt.edu.vn>
2024-07-20 15:33:52 +0700

    slideshow

--- src/components/customer-screen/slideshow.tsx
+++ src/components/customer-screen/slideshow.tsx
@@ -3,21 +3,24 @@ import { Carousel } from 'antd'
 import slideShow1 from '@/assets/img/slideshow/31424.jpg' // Ensure paths are correct
 import slideShow2 from '@/assets/img/slideshow/srcset.jpeg' // Example additional image
 import 'src/scss/components/_slideshow.scss' // Import the SCSS file
+import { useViewPromotionList } from '@/features/manager-feature/promotion-mng/view-promotion/use-view-promotion-list'
 
 const images = [slideShow1, slideShow2] // Add your image sources here
 
 const SlideShow: React.FC = () => {
   const carouselRef = React.useRef<any>(null)
+  const { data: promotion } = useViewPromotionList()
+  console.log(promotion)
 
   return (
     <div className="home__container relative">
       <div className="slideshow">
         <Carousel autoplay ref={carouselRef} className="slideshow__inner">
-          {images.map((image, index) => (
-            <div key={index} className="slideshow__item">
+          {promotion?.map((promote: any, index: any) => (
+            <div key={promote?.promotionId} className="slideshow__item">
               <picture>
-                <source media="(max-width: 767.98px)" srcSet={image} />
-                <img src={image} alt={`Slide ${index}`} className="slideshow__img" />
+                <source media="(max-width: 767.98px)" srcSet={promote?.promotionImg} />
+                <img src={promote?.promotionImg} alt={`Slide ${index}`} className="slideshow__img" />
               </picture>
             </div>
           ))}
